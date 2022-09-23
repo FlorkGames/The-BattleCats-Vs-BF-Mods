a03513996d8119293cedb75490940ec4199c7923
FlorkGames <105853494+FlorkGames@users.noreply.github.com>
2022-09-23 19:32:56 -0300

    Add files via upload

---
+++ assets/preload/data/to-the-unknown-world/mechanic.lua
@@ -0,0 +1,256 @@
+local canAttack = true
+local Hwalk = false
+local HKB = 0
+local Swalk = false
+local SKB = 0
+local Sattacked = 0
+
+function onSongStart()
+   noteTweenAlpha('nonote1', 0, 0, 1, 'cubeInOut')
+   noteTweenAlpha('nonote2', 1, 0, 1, 'cubeInOut')
+   noteTweenAlpha('nonote3', 2, 0, 1, 'cubeInOut')
+   noteTweenAlpha('nonote4', 3, 0, 1, 'cubeInOut')
+
+   runTimer('Hippwalk', getRandomInt(1, 32), 1)
+   runTimer('Saewalk', getRandomInt(4, 36), 1)   
+end
+
+
+function onCreate()
+      makeAnimatedLuaSprite('hippo', 'characters/Hippo_Alien', -600, 600)
+	  addAnimationByPrefix('hippo', 'Idle','Idle', 1, true)
+	  addAnimationByPrefix('hippo', 'Walk','Walk', 8, true)
+	  addAnimationByPrefix('hippo', 'Attack','Attack', 8, false)
+      addLuaSprite('hippo', true)
+	  scaleObject('hippo', 3.5, 3.5)
+
+      makeAnimatedLuaSprite('sae', 'characters/Sael_Alien', -640, 700)
+	  addAnimationByPrefix('sae', 'Idle','Idle', 1, true)
+	  addAnimationByPrefix('sae', 'Walk','Walk', 8, true)
+	  addAnimationByPrefix('sae', 'Attack','Attack', 8, false)
+      addLuaSprite('sae', true)
+	  scaleObject('sae', 3.2, 3.2)	  
+	  
+	  makeLuaText('cantAtt', 'You can`t attack', 300, 430, 85)
+	  addLuaText('cantAtt', true)
+	  setTextSize('cantAtt', 30)
+	  setProperty('cantAtt.visible', false)
+
+	if getPropertyFromClass('ClientPrefs', 'downScroll') then
+	   setProperty('cantAtt.y', 605)
+    end	
+	makeAnimatedLuaSprite('attackB', 'virtualbuttons', 0, 582.5);
+	addAnimationByPrefix('attackB', 'anonPress', 'anonPress', 24, false);
+	addAnimationByPrefix('attackB', 'aPressed', 'aPressed', 12, false);
+	addLuaSprite('attackB', false);
+	setObjectCamera('attackB', 'other');	
+end
+
+function onUpdate()
+  if Hwalk == false then
+  elseif Hwalk == true then
+     setProperty('hippo.x', getProperty('hippo.x')+ 2)
+     objectPlayAnimation('hippo', 'Walk', false)	 
+  end
+
+  if getProperty('hippo.x') > getProperty('boyfriend.x')- 345 and Hwalk == true then
+     Hwalk = false
+     objectPlayAnimation('hippo', 'Idle', false)		 
+	 runTimer('Hippattack', 2, 1)
+  end
+  
+  if Hattack == true then
+     Hattack = false
+     	objectPlayAnimation('hippo', 'Attack', false)
+	      if getProperty('hippo.animation.curAnim.finished') and getProperty('hippo.animation.curAnim.name') == 'Attack' and Hattack == true then
+		    setProperty('healthBar.percent', getProperty('healthBar.percent')- 0.5)		
+		     	objectPlayAnimation('hippo', 'Idle', false)	
+			    runTimer('Hippattack', 2, 1)
+		  end
+  end
+  
+ 	 if getProperty('hippo.animation.curAnim.finished') and getProperty('hippo.animation.curAnim.name') == 'Attack' then		
+		 setProperty('health', getProperty('health')- 0.5)	 
+		 characterPlayAnim('boyfriend', 'hurt', 24, false)	
+		 objectPlayAnimation('hippo', 'Idle', false)			  
+         runTimer('Hippattack', 1, 1)	
+         playSound('attack', 1, 1)		 
+	 end 
+	
+    if Hknockback == true then
+       cancelTimer('Hippattack')
+       setProperty('hippo.x', getProperty('hippo.x')- 12)	
+       objectPlayAnimation('hippo', 'Idle', false)	   
+	   setProperty('hippo.angle', -25)		    
+       if Hknockback == true and getProperty('hippo.x') < -600 then
+         Hknockback = false	
+		 Hwalk = true
+		 setProperty('hippo.angle', 0)
+       end	
+    end
+	
+	  if getProperty('hippo.y') == 500 then
+	     doTweenY('hipk', 'hippo', 600, 0.6, 'bounceOut')	 
+	  end
+	
+	if HKB == 2 then
+	   Hwalk = false
+	   Hknockback = false
+	   HKB = 0
+	   setProperty('hippo.x', -600)
+	   setProperty('hippo.angle', 0)	   
+	   playSound('dead', 1, 1)	   
+	   runTimer('Hippwalk', getRandomInt(1,32), 1)
+	   runTimer('Hippscore', 0.01, 1)	   
+	end
+	
+  if Swalk == false then
+  elseif Swalk == true then
+     setProperty('sae.x', getProperty('sae.x')+ 1)
+     objectPlayAnimation('sae', 'Walk', false)	 
+  end
+
+  if getProperty('sae.x') > getProperty('boyfriend.x')- 385 and Swalk == true then
+     Swalk = false
+     objectPlayAnimation('sae', 'Idle', false)		 
+	 runTimer('Saeattack', 5, 1)
+  end  
+  
+  	 if getProperty('sae.animation.curAnim.finished') and getProperty('sae.animation.curAnim.name') == 'Attack' then		
+		 setProperty('health', getProperty('health')- 1)	 
+		 characterPlayAnim('boyfriend', 'hurt', 24, false)	
+		 objectPlayAnimation('sae', 'Idle', false)			 
+         triggerEvent('Change Scroll Speed', 0.5, 0.5)	
+         runTimer('SaeabilityEnd', 4, 1)			 
+         runTimer('Saeattack', 5, 1)	
+         playSound('attack', 1, 1)		 
+	 end  
+	 
+    if Sknockback == true then
+       cancelTimer('Saeattack')
+       setProperty('sae.x', getProperty('sae.x')- 12)	
+       objectPlayAnimation('sae', 'Idle', false)	   
+	   setProperty('sae.angle', -25)		     	   
+       if Sknockback == true and getProperty('sae.x') < -640 then
+         Sknockback = false	
+		 Swalk = true
+		 setProperty('sae.angle', 0)
+       end	
+    end
+	
+	  if getProperty('sae.y') == 600 then
+	     doTweenY('saeDOWN', 'sae', 700, 0.6, 'bounceOut')	 
+	  end		  
+	  
+	if Sattacked == 2 then
+	  runTimer('Skb', 0.75, 1)
+      Sknockback = true	
+	  Sattacked = 0
+      doTweenY('saeUP', 'sae', 600, 0.35, 'cubeOut')		  
+	end 
+	
+	if SKB == 2 then
+	   Swalk = false
+	   Sknockback = false
+	   SKB = 0
+       objectPlayAnimation('sae', 'Idle', false)	   
+	   setProperty('sae.x', -640)
+	   setProperty('sae.angle', 0)	   
+	   playSound('dead', 1, 1)	   
+	   runTimer('Saewalk', getRandomInt(4,36), 1)
+	   runTimer('Saescore', 0.01, 1)
+	end	  	
+	  
+	
+  if getPropertyFromClass('flixel.FlxG', 'keys.pressed.SHIFT') then
+     doTweenZoom('cam', 'camGame', 0.85, 2.5, 'cubeInOut')
+  end	
+  if getPropertyFromClass('flixel.FlxG', 'keys.pressed.CONTROL') then
+     doTweenZoom('cam', 'camGame', 0.95, 2.5, 'cubeInOut')
+  end	  
+	   
+  if canAttack == true and (getMouseX('camHUD') > 0 and getMouseX('camHUD') < 128) and (getMouseY('camHUD') > 582.5 and getMouseY('camHUD') < 720 and mouseReleased('left')) then
+     characterPlayAnim('boyfriend', 'attack', 16, true)
+	 runTimer('cooldownBF', 0.01, 1)
+  end
+  
+  if canAttack == true and (getMouseX('camHUD') > 0 and getMouseX('camHUD') < 128) and (getMouseY('camHUD') > 582.5 and getMouseY('camHUD') < 720 and mouseReleased('left')) and getProperty('hippo.x') > getProperty('boyfriend.x')- 385 then
+     characterPlayAnim('boyfriend', 'attack', 16, true)
+	 playSound('bfattack', 1, 1)
+     doTweenY('hipb', 'hippo', 500, 0.35, 'cubeOut')	 	 
+	 Hknockback = true 
+	 runTimer('Hkb', 0.75, 1)
+	 runTimer('cooldownBF', 0.01, 1)
+  end 
+  
+  if canAttack == true and (getMouseX('camHUD') > 0 and getMouseX('camHUD') < 128) and (getMouseY('camHUD') > 582.5 and getMouseY('camHUD') < 720 and mouseReleased('left')) and getProperty('sae.x') > getProperty('boyfriend.x')- 385 then
+     characterPlayAnim('boyfriend', 'attack', 16, true)
+	 playSound('bfattack', 1, 1)	 
+	 runTimer('Saeattacked', 0.1, 1)
+	 runTimer('cooldownBF', 0.01, 1)
+  end   
+  
+    if (getMouseX('camHUD') > 0 and getMouseX('camHUD') < 128) and (getMouseY('camHUD') > 582.5 and getMouseY('camHUD') < 720 and mouseReleased('left')) then
+  		objectPlayAnimation('dodge', 'aPressed', false);
+	else
+		objectPlayAnimation('dodge', 'anonPress', false);
+	end
+  
+  if canAttack == false then
+    setProperty('cantAtt.visible', true)
+  end	
+end
+
+
+function onTimerCompleted(tag, loops, loopsLeft)
+       --Hippo Alien Timers--
+     if tag == 'Hippwalk' then
+	    Hwalk = true
+	 end	
+	 if tag == 'Hippattack' then
+     	objectPlayAnimation('hippo', 'Attack', false)				
+	 end
+	 if tag == 'Hkb' then
+     	HKB = HKB + 1		
+	 end	 
+	 if tag == 'Hippscore' then
+	    setProperty('songScore', getProperty('songScore')+ getRandomInt(7000, 9550))
+     end	   
+       --Sael Alien Timers--	 
+     if tag == 'Saewalk' then
+	    Swalk = true
+	 end	
+	 if tag == 'Saeattack' then
+     	objectPlayAnimation('sae', 'Attack', false)				
+	 end
+	 if tag == 'Skb' then
+     	SKB = SKB + 1		
+	 end	 
+	 if tag == 'Saeattacked' then
+	    Sattacked = Sattacked + 1	 
+	 end
+     if tag == 'SaeabilityEnd' then
+        triggerEvent('Change Scroll Speed', 1, 4)	
+     end		 
+	 if tag == 'Saescore' then
+	    setProperty('songScore', getProperty('songScore')+ getRandomInt(11500, 15150))	  
+     end	   
+	 
+	 if tag == 'cooldownBF' then
+	    canAttack = false
+		setProperty('cantAtt.color', getColorFromHex('878787'))
+		doTweenColor('canAtt', 'cantAtt', 'FFFFFF', 3, 'bounceOut')
+		runTimer('cooldownBFcomplete', 3, 1)
+	 end
+	 if tag == 'cooldownBFcomplete' then
+	    canAttack = true
+	    setProperty('cantAtt.visible', false)		
+	 end
+end
+
+function onEndSong()
+   cancelTimer('Saeattack')
+   cancelTimer('Saewalk')
+   cancelTimer('Hippattack')
+   cancelTimer('Hippwalk')   
+end
